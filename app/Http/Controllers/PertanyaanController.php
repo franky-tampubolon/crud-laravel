37dddfcf319e7e2f665a422bd97943867435a5f7
franky-tampubolon <67570602+franky-tampubolon@users.noreply.github.com>
2020-07-04 13:07:24 +0700

    Add files via upload

--- app/Http/Controllers/PertanyaanController.php
+++ app/Http/Controllers/PertanyaanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Pertanyaan;
+use App\Jawaban;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -63,4 +64,57 @@ class PertanyaanController extends Controller
         return redirect('/pertanyaan')->with('error', 'Pertanyaan gagal ditambahkan');
     }
 
+    public function edit(Request $request)
+    {
+        $pertanyaan = Pertanyaan::findOrFail($request->id);
+        return view('pertanyaan.edit',[
+            'title'     => "Edit Pertanyaan",
+            'pertanyaan'    => $pertanyaan
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $messages = [
+            'required' => ':attribute harus diisi.'            
+        ];
+
+        //rules validasi inputan user
+        Validator::make($request->all(), [
+            'judul' => 'required',
+            'isi' => 'required'
+        ], $messages)->validate();
+
+        // jika lolos verifikasi insert ke database
+        $pertanyaan = Pertanyaan::where('id', $request->id)->update([
+            'judul'             => $request->judul,
+            'isi_pertanyaan'    => $request->isi,
+        ]);
+        if($pertanyaan){
+            return redirect('/pertanyaan')->with('success', 'Pertanyaan berhasil diupdate');
+        }
+        return redirect('/pertanyaan')->with('error', 'Pertanyaan gagal diupdate');
+    }
+
+    public function show(Request $request)
+    {
+        $pertanyaan = Pertanyaan::findOrFail($request->id);
+        // dd($pertanyaan);
+        $jawaban = Jawaban::where('pertanyaan_id', $request->id)->get();
+        return view('pertanyaan.detail', [
+            'pertanyaan'    =>$pertanyaan,
+            'jawaban'       => $jawaban,
+            'title' => "Lihat Pertanyaan"
+        ]);
+    }
+
+    public function destroy(Request $request)
+    {
+        $pertanyaan = Pertanyaan::findOrFail($request->id);
+        $hasil = $pertanyaan->delete();
+        if($hasil){
+            return redirect('/pertanyaan')->with('success', 'Pertanyaan berhasil dihapus');
+        }
+        return redirect('/pertanyaan')->with('error', 'Pertanyaan gagal dihapus');
+    }
 }
